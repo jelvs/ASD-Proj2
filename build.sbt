a0a1f864894ba7444954e94e78e5b263bd0c134e
jelvs <jelvs13@gmail.com>
2018-12-05 17:24:10 +0000

    first commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "ASD-Proj2"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.5.17",
+  "com.typesafe.akka" %% "akka-testkit" % "2.5.17" % Test,
+  "com.typesafe.akka" %% "akka-remote" % "2.5.18"
+) 
---
+++ src/main/scala/app/process.scala
@@ -0,0 +1,50 @@
+package app
+
+import akka.actor.{ActorSystem, Props}
+import com.typesafe.config.{Config, ConfigFactory, ConfigValueFactory}
+
+
+
+object Process extends App {
+
+  var port = 2552
+  if (args.length != 0) {
+    port = args(0).toInt
+  }
+
+  val config = configure()
+  val system = ActorSystem("SystemName", config)
+  val ownAddress = getOwnAddress(port)
+
+
+  var contactNode = ""
+  if (args.length > 1) {
+    contactNode =  s"akka.tcp://${system.name}@${args(1)}"
+    println("Contact: " + contactNode)
+  }
+
+  println(ownAddress)
+
+
+
+
+  def configure(): Config = {
+
+    ConfigFactory.load.getConfig("Process").withValue("akka.remote.netty.tcp.port",
+      ConfigValueFactory.fromAnyRef(port))
+
+  }
+
+  def getOwnAddress(port: Int) = {
+    val address = config.getAnyRef("akka.remote.netty.tcp.hostname")
+    val port = config.getAnyRef("akka.remote.netty.tcp.port")
+
+    s"akka.tcp://${system.name}@${address}:${port}"
+  }
+
+
+
+
+
+}
+
